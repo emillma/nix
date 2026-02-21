9ff02c73a2df531ed363f77de87d4ffd8ee0e9a5
Emil Mårtens <emil.martens@gmail.com>
2026-02-21 21:52:43 +0100

    Switch to Plasma 6 and clean up GDM/GNOME configuration

--- modules/system/display.nix
+++ modules/system/display.nix
@@ -4,34 +4,24 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
-  # Reduce top bar size and UI scaling
-  services.desktopManager.gnome.extraGSettingsOverrides = ''
-    [org.gnome.shell]
-    enabled-extensions=['tilingshell@ferrarodomenico.com', 'just-perfection-desktop@just-perfection']
+  # Enable Wayland for SDDM and configure HiDPI settings
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.settings = {
+    Wayland = {
+      EnableHiDPI = true;
+    };
+  };
 
-    [org.gnome.shell.extensions.just-perfection]
-    panel-size=24
-
-    [org.gnome.desktop.interface]
-    text-scaling-factor=0.9
-  '';
+  # Install SDDM-KCM to allow saving monitor layouts to the login screen
+  environment.systemPackages = [ pkgs.kdePackages.sddm-kcm ];
 
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-
-  systemd.tmpfiles.rules = [
-    # Remove existing file if present
-    "r /run/gdm/.config/monitors.xml - - - - -"
-    # Copy the GDM-specific monitors.xml (use C for copy, not symlink)
-    "C /run/gdm/.config/monitors.xml - - - - ${./monitors-gdm.xml}"
-    # Recursively set ownership on the .config dir to gdm:gdm
-    "Z /run/gdm/.config - gdm gdm - -"
-  ];
 }
