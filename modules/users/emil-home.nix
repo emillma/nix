2891b13ee37145959bb9753e600b21a1d6802c0b
Emil Mårtens <emil.martens@gmail.com>
2026-02-21 14:48:45 +0100

    Add Home Manager and manage Zed keybindings

---
+++ modules/users/emil-home.nix
@@ -0,0 +1,32 @@
+```{ config, pkgs, ... }:
+
+{
+  home.username = "emil";
+  home.homeDirectory = "/home/emil";
+
+  # Home Manager state version (matches system version)
+  home.stateVersion = "25.11";
+
+  # Manage Zed keybindings via XDG config
+  xdg.configFile."zed/keymap.json".text = builtins.toJSON [
+    {
+      "context" = "Workspace";
+      "bindings" = { };
+    }
+    {
+      "context" = "Editor && vim_mode == insert";
+      "bindings" = { };
+    }
+    {
+      "context" = "Terminal";
+      "bindings" = {
+        "ctrl-c" = "terminal::Copy";
+        "ctrl-v" = "terminal::Paste";
+        "ctrl-shift-c" = [ "terminal::SendKeystroke" "ctrl-c" ];
+      };
+    }
+  ];
+
+  # Basic Home Manager setup
+  programs.home-manager.enable = true;
+}
