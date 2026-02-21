ed6bccb444282364bf1ac9bca6f086e40de229bc
Emil Mårtens <emil.martens@gmail.com>
2026-02-21 15:02:04 +0100

    Remove Home Manager and keep Zed settings manual

--- flake.nix
+++ flake.nix
@@ -4,25 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     zed.url = "github:zed-industries/zed";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, zed, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, zed, ... }@inputs: {
     nixosConfigurations.workstation = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./machines/workstation/default.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          home-manager.users.emil = import ./modules/users/emil-home.nix;
-        }
       ];
     };
   };
